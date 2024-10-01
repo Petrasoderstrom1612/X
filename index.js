402f027a1f30eb2d508dd872cd19a3a1cdfe63ac
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-10-01 10:20:11 +0200

    added a big for of and in it a small for of loop in the function to create html. both thave variables declared separatelly prior to those loops so I just reassign variable, the dom happens after both loop and loop within

--- index.js
+++ index.js
@@ -6,3 +6,55 @@ tweetBtn.addEventListener("click", function(){
     console.log(myInput.value)
     myInput.value =""
 })
+
+
+
+function getFeedHtml(tweets){
+let tweetsection = ""
+let comments = ""
+for (let oneTweet of tweets){
+    if (oneTweet.replies.length > 0){ //DEEPER LOOP TO GET OBJECTS IN THE ARRAY OF REPLIES IN EVERY TWEET
+        for (let oneReply of oneTweet.replies){
+            comments += `
+            <div>
+                <p>${oneReply.handle}</p>
+                <img src="${oneReply.profilePic}"/>
+                <p>${oneReply.tweetText}</p>
+            </div>
+            
+            `
+        }
+            
+    }
+                        //THE BIG LOOP TO LOOP OVER ALL OBJECTS, note ${comments} where I place the deeper loop's variable
+    tweetsection += ` 
+    <div class="tweet">
+    <div class="tweet-inner">
+        <img src="${oneTweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${oneTweet.handle}</p>
+            <p class="tweet-text">${oneTweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">${oneTweet.replies.length} </span>
+                </br>
+                <span class="tweet-detail">
+                    ${oneTweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    ${oneTweet.retweets}
+                </span>
+                <p>
+                ${comments} 
+                </p>
+            </div>   
+        </div>            
+    </div>
+</div>
+    `
+    }
+feed.innerHTML = tweetsection
+}
+    
+getFeedHtml(tweetsData)
+
+

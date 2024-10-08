1c57c93728625c0a4a714726276fe89addb8e647
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-10-08 11:51:12 +0200

    improved comments

--- index.js
+++ index.js
@@ -2,30 +2,42 @@ import {tweetsData} from "./data.js"
 const tweetBtn = document.getElementById("tweet-btn")
 const myInput = document.getElementById("my-input")
 
+
 tweetBtn.addEventListener("click", function(){
     console.log(myInput.value)
     myInput.value =""
 })
 
-function getFeedHtml(tweets){
-let tweetsection = ""
-let comments = ""
-tweets.forEach((oneTweet) =>{         //I USED FOREACH IN THE FIRST FORLOOP TO HAVE VARIATION
+function getFeedHtml(tweets){ //THE HTML CREATOR
+    let tweetsection = ""
+    let comments = ""
+    
+    tweets.forEach((oneTweet) =>{         //I USED FOREACH IN THE FIRST BIG FORLOOP TO HAVE VARIATION
+    let heartsClass = ""
+    let retweetClass = ""
+
+    if(oneTweet.isLiked){
+        heartsClass = "liked"
+    }
+    if(oneTweet.isRetweeted){
+        retweetClass = "retweeted"
+    }
+    
     if (oneTweet.replies.length > 0){ //DEEPER LOOP IN IF TO GET OBJECTS IN THE ARRAY OF REPLIES IN EVERY TWEET INSIDE OF A VARIABLE
-        for (let oneReply of oneTweet.replies){
-            comments += `
-            <div>
+            for (let oneReply of oneTweet.replies){
+                comments += `
+                <div>
                 <p>${oneReply.handle}</p>
                 <img src="${oneReply.profilePic}"/>
                 <p>${oneReply.tweetText}</p>
-            </div>
-            
-            `
+                </div>
+                
+                `
         }
             
     }
     
-    tweetsection +=  //THE BIG LOOP TO LOOP OVER ALL OBJECTS, note ${comments} where I place the deeper loop's variable
+    tweetsection +=  // creating all HTML, note ${comments} where I place the deeper loop's variable
     ` 
     <div class="tweet">
         <div class="tweet-inner">
@@ -37,10 +49,10 @@ tweets.forEach((oneTweet) =>{         //I USED FOREACH IN THE FIRST FORLOOP TO H
                     <span class="tweet-detail"><i class="fa-regular fa-comment-dots" data-replies="${oneTweet.uuid}"></i>${oneTweet.replies.length} </span>
                     </br>
                     <span class="tweet-detail">
-                       <i class="fa-regular fa-heart" data-hearts="${oneTweet.uuid}"></i> ${oneTweet.likes}
+                       <i class="fa-solid fa-heart ${heartsClass}" data-hearts="${oneTweet.uuid}"></i> ${oneTweet.likes}
                     </span>
                     <span class="tweet-detail">
-                        <i class="fa-solid fa-retweet" data-retweets="${oneTweet.uuid}"></i>${oneTweet.retweets}
+                        <i class="fa-solid fa-retweet ${retweetClass}" data-retweets="${oneTweet.uuid}"></i>${oneTweet.retweets}
                     </span>
                     <p>
                     ${comments} 
@@ -62,31 +74,23 @@ render(tweetsData)
 
 
 document.addEventListener("click",(e) => { // 3 LISTENERS ON ICON CLICKS VIA DATASET, FUNCTIONS LISTED BELOW
-    if(e.target.dataset.replies){
-        detectReply(e.target.dataset.replies)
-    }
-    else if(e.target.dataset.hearts){
+    if(e.target.dataset.hearts){
         handleLike(e.target.dataset.hearts)
     }
     else if(e.target.dataset.retweets){
         handleRetweet(e.target.dataset.retweets)
     }
+    else if(e.target.dataset.replies){
+        detectReply(e.target.dataset.replies)
+    }
     render(tweetsData)  //taking advantage of shallow copy to just reassign icons as the original data.js changes when shallow copy is created
 })
 
-function detectReply(tweetUuid){
-    const targetTweetObj = tweetsData.filter((tweet) => {
-        return tweet.uuid === tweetUuid                     //when you filter, you want to return truth or false
-    })[0]                                                   //filter method iterates over an array of objects and returns a shallow copy with a filtered out array, if you want to return object, then use [] 
-    targetTweetObj.replies.push(1)
-    console.log(targetTweetObj)
-    console.log(tweetsData)                                 //when modifying advanced data types such as objects and arrays, you modify the type on the stack, in other words, the original changes
-}
 
 function handleLike(tweetUuid){ 
     const targetTweetObj = tweetsData.filter(function(tweet){
         return tweet.uuid === tweetUuid
-    })[0]
+    })[0] //shallow copy of tweetsData object created, it means the original data is modifiable with every new change
 
     if (targetTweetObj.isLiked){
         targetTweetObj.likes--
@@ -98,7 +102,7 @@ function handleLike(tweetUuid){
 }
 
 function handleRetweet(tweetUuid){
-    tweetsData.map((tweet) => {
+    tweetsData.map((tweet) => { //shallow copy of tweetsData object created, it means the original data is modifiable with every new change
         if (tweetUuid === tweet.uuid){
             if(tweet.isRetweeted){
                 tweet.retweets --
@@ -110,7 +114,17 @@ function handleRetweet(tweetUuid){
     })
 }
 
-// function detectLike(tweetUuid){
+function detectReply(tweetUuid){
+    const targetTweetObj = tweetsData.filter((tweet) => {
+        return tweet.uuid === tweetUuid                     //when you filter, you want to return truth or false
+    })[0]                                                   //filter method iterates over an array of objects and returns a shallow copy with a filtered out array, if you want to return object, then use [] 
+    targetTweetObj.replies.push(1)
+    console.log(targetTweetObj)
+    console.log(tweetsData)                                 //when modifying advanced data types such as objects and arrays, you modify the type on the heap, in other words, the original changes
+}
+
+
+// function handleLike(tweetUuid){                         // just showing the variation you can have in forloops
 //         for (let i =0; i < tweetsData.length;i++){
 //             if(tweetsData[i].uuid === tweetUuid && !tweetsData[i].isLiked){
 //                 tweetsData[i].likes ++
